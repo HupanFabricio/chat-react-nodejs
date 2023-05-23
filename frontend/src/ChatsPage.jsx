51e774f50683adebbc6d68b8e3887f0ccba7c893
HupanFabricio <fabrihupan@gmail.com>
2023-05-23 13:19:30 -0300

    Finish

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,16 @@
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
+
+const ChatsPage = (props) => {
+    return (
+        <div style={{ height: '100vh' }}>
+            <PrettyChatWindow
+                projectId='ef793a49-94f3-40cb-9cbb-dbdaff45556f'
+                username={props.user.username}
+                secret={props.user.secret}
+                style={{ height: '100%' }}
+            />
+        </div>
+    )
+}
+
+export default ChatsPage
